26ebc1208f90645cd78c369c1f524b824dc5c924
Yushi Sato <yushi.kinger@gmail.com>
2021-12-23 23:41:59 +0900

    footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,15 +38,10 @@ const Home: NextPage = () => {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/0xys/etherhex"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        >@0xys</a>
       </footer>
     </div>
   )
